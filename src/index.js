1cc9767928fb92cdfd1d3ac5905128ae3c0f6455
Kaesid <keskaesid@gmail.com>
2021-03-01 15:20:40 +0400

    feat: add functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
+exports.min = function min(array) {
+    if (array === undefined || +array === 0) return 0;
+    let a = [];
+    typeof array !== "object" ? (a = Array.from(arguments)) : (a = array);
+    return a.sort((x, y) => x - y)[0];
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (array === undefined || +array === 0) return 0;
+    let a = [];
+    typeof array !== "object" ? (a = Array.from(arguments)) : (a = array);
+    return a.sort((x, y) => y - x)[0];
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (array === undefined || +array === 0) return 0;
+    let a = [];
+    typeof array !== "object" ? (a = Array.from(arguments)) : (a = array);
+    return a.reduce((a, b) => a + b, 0) / a.length;
+};
